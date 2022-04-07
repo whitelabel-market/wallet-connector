97f81378a36c0716f3b7fff0c58af5ef5ad32a29
Fabian Piper <fabianpiper@web.de>
2022-04-07 08:36:34 +0200

    Set vite library mode. Init codestyle.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/components/HelloWorld.vue"),
+            name: "wallet-connector",
+            fileName: (format) => `wallet-connector.${format}.js`,
+        },
+        rollupOptions: {
+            external: ["vue"],
+            output: {
+                // Provide global variables to use in the UMD build
+                // Add external deps here
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
+    },
+    plugins: [vue()],
+});
