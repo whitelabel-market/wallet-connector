97f452557cf7a38fe6bf6ec7a19e8fd711154355
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-05-12 02:12:28 +0200

    fix: fix lint and tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,8 @@ module.exports = {
     root: true,
     env: {
         node: true,
-        "vue/setup-compiler-macros": true,
     },
-    extends: [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/typescript/recommended",
-        "plugin:prettier/recommended",
-        "prettier",
-    ],
+    extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
     parserOptions: {
         ecmaVersion: 2020,
     },
