5a742e8ebb4656c6f147455d3cefc5fd0c45f394
Kevin Hertwig <hertwig@campus.tu-berlin.de>
2022-05-22 03:44:49 +0200

    chore(index): export all types and modules from entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import Core from "./core";
 import Providers from "./providers";
-import type { IProvider, ConnectorUserOptions, ProviderType } from "./types";
+import type {
+    IProvider,
+    ConnectorUserOptions,
+    ConnectorOptions,
+    ProviderType,
+    ConnectFn,
+    ConnectResult,
+} from "./types";
 
 const { Connector } = Core;
 
-export type { IProvider, ConnectorUserOptions, ProviderType };
+export type {
+    IProvider,
+    ConnectorUserOptions,
+    ProviderType,
+    ConnectorOptions,
+    ConnectFn,
+    ConnectResult,
+};
 export { Connector, Providers };
